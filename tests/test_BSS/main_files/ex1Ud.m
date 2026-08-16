d9dbf98eabbed3b454051e2837167189eac04a60
Vikram Singh <128346776+vikrasingh@users.noreply.github.com>
2026-08-16 17:33:09 -0500

    initial commit

---
+++ tests/test_BSS/main_files/ex1Ud.m
@@ -0,0 +1,55 @@
+
+function  [fbest_all_inp_files,cpu_all_inp_files,niter_all_inp_files,stopflag_all_inp_files]=ex1Ud
+% 03/20/24, give the input file pairs to run below
+
+% Each row of InputFiles below represents a pair of algo. and example input file to run a test
+% Algo. input file first and example input file second, order of input files in a row matters
+para_struct.mainfile=mfilename('fullpath');
+para_struct.toDebug=0;  % =0 no intermediate output, =1 some intermediate output, =2 the most extensive debugging
+para_struct.id='Ex1Ud';
+InputFiles={
+ %5 UD, snr 0.05
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One4Ex20t80Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex200a300Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex400a500Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex800Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1000Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1500Ud2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex2000Ud2TmCcRh0p8Sn0p05'
+  
+ %6 UD, snr 0.5
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One4Ex20t80Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex200a300Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex400a500Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex800Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1000Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1500Ud2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex2000Ud2TmCcRh0p8Sn0p5'
+  
+ %7 UD, snr 1 
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One4Ex20t80Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex200a300Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex400a500Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex800Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1000Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1500Ud2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex2000Ud2TmCcRh0p8Sn1'
+  
+ %8 UD, snr 5 
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One4Ex20t80Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex200a300Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One2Ex400a500Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex800Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1000Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex1500Ud2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Ud',  'One1Ex2000Ud2TmCcRh0p8Sn5'
+ 
+           };   
+
+[fbest_all_inp_files,cpu_all_inp_files,niter_all_inp_files,stopflag_all_inp_files]=get_ex_data(InputFiles,para_struct);
+
+end % end 
+
+
+
+
---
+++ tests/test_BSS/main_files/ex2Od.m
@@ -0,0 +1,60 @@
+
+function  [fbest_all_inp_files,cpu_all_inp_files,niter_all_inp_files,stopflag_all_inp_files]=ex2Od
+% 03/20/24, give the input file pairs to run below
+if isunix % if running on cluster
+   addpath(genpath('/home/x-vsingh7/bss_suboptimal/'));  
+   addpath(genpath('/home/x-vsingh7/MinimizeQuadFunOverBox_c0_2023-08-21/'));
+end
+% Each row of InputFiles below represents a pair of algo. and example input file to run a test
+% Algo. input file first and example input file second, order of input files in a row matters
+para_struct.mainfile=mfilename('fullpath');
+para_struct.to_debug=0;  % =0 no intermediate output, =1 some intermediate output, =2 the most extensive debugging
+para_struct.id='Ex2Od';
+InputFiles={
+ %   
+  %1 OD, snr 0.05  , 32 egs for p 20 to 5k for Sn 0p05
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two4Ex20t80Od2TmCcRh0p8Sn0p05'    
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex200a300Od2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex400a500Od2TmCcRh0p8Sn0p05'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex800Od2TmCcRh0p8Sn0p05v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1000Od2TmCcRh0p8Sn0p05v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1500Od2TmCcRh0p8Sn0p05v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex2000Od2TmCcRh0p8Sn0p05v2'
+  
+  %2 OD,  snr 0.5
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two4Ex20t80Od2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex200a300Od2TmCcRh0p8Sn0p5' 
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex400a500Od2TmCcRh0p8Sn0p5'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex800Od2TmCcRh0p8Sn0p5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1000Od2TmCcRh0p8Sn0p5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1500Od2TmCcRh0p8Sn0p5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex2000Od2TmCcRh0p8Sn0p5v2'
+  
+  %3 OD, snr 1
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two4Ex20t80Od2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex200a300Od2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex400a500Od2TmCcRh0p8Sn1'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex800Od2TmCcRh0p8Sn1v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1000Od2TmCcRh0p8Sn1v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1500Od2TmCcRh0p8Sn1v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex2000Od2TmCcRh0p8Sn1v2'
+  
+ %4 OD, snr 5 
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two4Ex20t80Od2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex200a300Od2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two2Ex400a500Od2TmCcRh0p8Sn5'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex800Od2TmCcRh0p8Sn5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1000Od2TmCcRh0p8Sn5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex1500Od2TmCcRh0p8Sn5v2'
+  'Mix99b3e9c9d3f3g3dV9Od',  'Two1Ex2000Od2TmCcRh0p8Sn5v2'
+  
+           };   
+% ppDimRunCall(InputFiles,paraStruct) % call parallel pair run for LLS 
+% pPairRunCall(InputFiles,para_struct);
+% pSetRunCall(InputFiles,paraStruct);
+[fbest_all_inp_files,cpu_all_inp_files,niter_all_inp_files,stopflag_all_inp_files]=get_ex_data(InputFiles,para_struct);
+end % end 
+
+
+
+
