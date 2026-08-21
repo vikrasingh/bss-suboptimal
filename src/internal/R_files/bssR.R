9b5983ad628274f85f1253ae165968de7ca6fcd9
Vikram Singh <128346776+vikrasingh@users.noreply.github.com>
2026-08-21 11:47:03 -0500

    Delete src/internal directory

--- src/internal/R_files/bssR.R
+++
@@ -1,9 +0,0 @@
-bssR <- function(x,y,k,cpulimit){
-  Packages <- c("ggplot2", "glmnet", "Matrix","bestsubset")  # packages to be loaded
-  lapply(Packages, library, character.only = TRUE)  # load all the packages
-  start.time=proc.time()    # to find the cputime
-  result_bss = bs(x,y,intercept=FALSE,k,time.limit=cpulimit,verbose=TRUE)
-  end.time=proc.time()
-  result_bss$cputime=as.double(end.time[2]-start.time[2])  # add cputime as a new field
-  return(result_bss)
-}
